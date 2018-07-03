86334e91650b12ba35cc5e10d1eb19765ddb9802
Sebastian Dittrich <sebastian.dittrich@live.de>
2018-07-03 22:51:18 +0200

    added programming languages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,9 +23,13 @@
             </div>
         </div>
 
-        <h2 class="header mt-16 container mx-auto">
-            Meine Projekte
-        </h2>
+        <div class="mt-16 container mx-auto p-4 text-grey-darker text-center">
+            Hallo, ich bin <span class="font-bold">Basti</span>. <br><br>
+            Ich bin Profi im Umgang mit <span class="font-bold">PHP</span>. <br><br>
+            Und ich mag <span class="font-bold">Laravel</span>. <br><br>
+            Ich entwickle nämlich <span class="font-bold">Web-Apps</span>. <br><br>
+            Hier ein paar <span class="font-bold">Beispiele</span>:
+        </div>
         <div class="flex justify-center items-stretch flex-wrap p-4 container mx-auto">
             <div class="project">
                 <h3 class="header">
@@ -60,6 +64,26 @@
                     <i class="mi mi-ChevronRight intext"></i>
                 </a>
             </div>
+        </div>
+
+        <div class="mt-8 container mx-auto p-4 text-grey-darker text-center">
+            Aber ich kann auch noch <span class="font-bold">mehr</span>. <br><br>
+            Zum Beispiel <span class="font-bold">KI's</span> programmieren. <br><br>
+            Oder <span class="font-bold">Server</span> managen. <br><br>
+            Zur besseren Übersicht, <br> alle meine <span class="font-bold">Programmiersprachen</span>.
+        </div>
+
+        <div class="flex flex-wrap justify-center items-center languages container mx-auto">
+            <img src="{{ asset('languages/php.png') }}" alt="">
+            <img src="{{ asset('languages/html.png') }}" alt="">
+            <img src="{{ asset('languages/css.png') }}" alt="">
+            <img src="{{ asset('languages/mysql.png') }}" alt="">
+            <img src="{{ asset('languages/javascript.png') }}" alt="">
+            <img src="{{ asset('languages/python.png') }}" alt="">
+            <img src="{{ asset('languages/rust.png') }}" alt="">
+            <img src="{{ asset('languages/c-sharp.png') }}" alt="">
+            <img src="{{ asset('languages/bash.png') }}" alt="">
+            <img src="{{ asset('languages/git.png') }}" alt="">
         </div>
 
         {{-- <div class="hero">
