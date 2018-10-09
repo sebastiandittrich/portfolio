293fe2aa0139eeef52d6ea562ae6d82f0fb99d89
Sebastian Dittrich <sebastian.dittrich@live.de>
2018-10-09 15:42:58 +0200

    Build dist

---
+++ dist/precache-manifest.baa8eba74afe9d176a93df25cb7951ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/fonts/feather.woff"
+  },
+  {
+    "revision": "90f969db293a9077b0555139e6c9b20e",
+    "url": "/fonts/feather.svg"
+  },
+  {
+    "revision": "4bf99403d1b447580d96",
+    "url": "/js/chunk-vendors.4cc7b623.js"
+  },
+  {
+    "revision": "a6a1121da86eca4bef62f5ae144b8e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/fonts/feather.ttf"
+  },
+  {
+    "revision": "f436a9e2bf6a46cf39a1",
+    "url": "/js/app.22c6f715.js"
+  },
+  {
+    "revision": "1a8dc29639a49db5ebb62abbdbcb53a9",
+    "url": "/img/artfamily.jpg"
+  },
+  {
+    "revision": "2befeb8c7d913caa6db60f782aee420f",
+    "url": "/img/asozialesnetzwerk.png"
+  },
+  {
+    "revision": "ef6349b4598daf9db05b26818994bca0",
+    "url": "/img/avatar.jpg"
+  },
+  {
+    "revision": "ba4f437af712d9c0bf63deec7875d9cd",
+    "url": "/img/todoninja.png"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/fonts/feather.eot"
+  },
+  {
+    "revision": "f436a9e2bf6a46cf39a1",
+    "url": "/css/app.6562381d.css"
+  }
+];
