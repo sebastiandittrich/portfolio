712d41b8db8718413aeda63c0321428debfdbf9f
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-03-16 22:09:49 +0000

    Fixed image loading detection

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view style="grid-area: content" v-slot="{ Component }">
     <Transition name="opacity">
-      <div v-if="imagesloaded" style="display: grid; grid-template: 'content' 1fr 'footer' auto / 100%" class="min-h-screen">
+      <div v-show="imagesloaded" style="display: grid; grid-template: 'content' 1fr 'footer' auto / 100%" class="min-h-screen">
         <component :is="Component"></component>
         <Footer style="grid-area: footer"></Footer>
       </div>
@@ -10,15 +10,15 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { nextTick, onMounted, ref } from "vue";
 import Footer from "./components/Footer.vue";
 
 const imagesloaded = ref(false);
 
-onMounted(() => {
+onMounted(async () => {
   const id = setInterval(() => {
     const imgs = Array.from(document.getElementsByTagName("img"));
-    const done = imgs.every((img) => img.complete);
+    const done = imgs.length > 0 && imgs.every((img) => img.complete && img.height > 0);
     if (done) {
       clearInterval(id);
       imagesloaded.value = true;
