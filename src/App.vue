4093caddc71c4e309ebc0f5be96283483377f60f
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-03-16 21:59:57 +0000

    Smooth first fade in

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts">
-import Footer from "./components/Footer.vue";
-</script>
-
 <template>
-  <div style="display: grid; grid-template: 'content' 1fr 'footer' auto / 100%" class="min-h-screen">
-    <router-view style="grid-area: content" />
-    <Footer style="grid-area: footer"></Footer>
-  </div>
+  <router-view style="grid-area: content" v-slot="{ Component }">
+    <Transition name="opacity">
+      <div v-if="imagesloaded" style="display: grid; grid-template: 'content' 1fr 'footer' auto / 100%" class="min-h-screen">
+        <component :is="Component"></component>
+        <Footer style="grid-area: footer"></Footer>
+      </div>
+    </Transition>
+  </router-view>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Footer from "./components/Footer.vue";
+
+const imagesloaded = ref(false);
+
+onMounted(() => {
+  const id = setInterval(() => {
+    const imgs = Array.from(document.getElementsByTagName("img"));
+    const done = imgs.every((img) => img.complete);
+    if (done) {
+      clearInterval(id);
+      imagesloaded.value = true;
+    }
+  }, 50);
+});
+</script>
