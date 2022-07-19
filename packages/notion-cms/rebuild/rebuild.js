dc6fb09a432805fd1a04db2503c16c8ada5b948b
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-07-19 14:57:55 +0000

    Added token

--- packages/notion-cms/rebuild/rebuild.js
+++ packages/notion-cms/rebuild/rebuild.js
@@ -1,8 +1,14 @@
 const fetch = require("node-fetch");
 
-exports.main = async () => {
+exports.main = async (args) => {
   const appId = process.env.APP_ID;
   const apiKey = process.env.API_KEY;
+  const rebuildToken = process.env.REBUILD_TOKEN;
+
+  if (args.token != rebuildToken) {
+    throw new Error("Invalid token");
+  }
+
   await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments`, {
     headers: {
       Authorization: `Bearer ${apiKey}`,
