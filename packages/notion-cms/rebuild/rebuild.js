caa3f2ff1ef71c4cefe35ec53306f11a8ba04e5b
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-07-19 14:51:33 +0000

    Add environment vars

--- packages/notion-cms/rebuild/rebuild.js
+++ packages/notion-cms/rebuild/rebuild.js
@@ -1,6 +1,15 @@
+const fetch = require("node-fetch");
+
 exports.main = async () => {
-  console.log(process.env);
-  return {
-    body: "Heheheha.",
-  };
+  const appId = process.env.APP_ID;
+  const apiKey = process.env.API_KEY;
+  await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments`, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: '{"force_build": true}',
+  });
+  return { body: "Done." };
 };
